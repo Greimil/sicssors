803cd1db0239fff63bacf46ab210f422e177a2f5
Greimil <greimilnunez@gmail.com>
2022-01-26 09:12:48 -0400

    click out bug fixed animation added

--- scrip.js
+++ scrip.js
@@ -5,18 +5,22 @@ let victoPlayer = 0
 let victocomputador = 0
 let respuesta
 
+
+
 const resultadoFinal = document.getElementById("ResultadoFinal")
 const rounds = document.getElementById("Rounds")
 const contadorRounds = document.getElementById("contadorRounds")
 const subpantalla = document.querySelector(".subpantalla")
 const scorePlayer = document.getElementById("score-player")
 const scoreComputer = document.getElementById("score-computer")
+const spanScreen = document.querySelector(".span-screen")
 
 
 
 window.addEventListener("click",(e) =>{
     
-    if(contador < 5 ){
+    if(contador < 5 && (e.target.id == "rock" || e.target.id == "paper" || e.target.id == "scissor" ) ){
+    
         playerSelection = e.target.id 
         computerSelection = computerPlay()
         respuesta = playRound(computerSelection,playerSelection)
@@ -24,6 +28,7 @@ window.addEventListener("click",(e) =>{
         contador++
         contadorRounds.textContent = contador
         
+        createAnimation(e.target.src, computerSelection)
         
         if(respuesta.includes("win")){
             victoPlayer++
@@ -35,21 +40,39 @@ window.addEventListener("click",(e) =>{
         scoreComputer.textContent = victocomputador
 
         
-        if(!playerSelection)return // no hacer nada si el user presiona fuera de las imagenes  
     }
     else{
-        if(victoPlayer > victocomputador){
+        if(victoPlayer > victocomputador && contador == 5){
             resultadoFinal.textContent = "You Win, you already played 5 rounds press F5 to play again"
-        } else if(victoPlayer < victocomputador){
+        } else if(victoPlayer < victocomputador && contador == 5 ){
             resultadoFinal.textContent = "You lose :( you already played 5 rounds press F5 to play again"
-        } else{
-            resultadoFinal.textContent = "It's a tie :)"
+        } else if (victoPlayer == victocomputador && victoPlayer > 0 && victocomputador > 0 ) {
+            resultadoFinal.textContent = "It's a tie :) press F5 to play again "
+        } else {
+            return
         }
         
     }
 
 
 })
+
+
+function createAnimation (playerSelection, computerSelection) {
+    
+    let srcComputer 
+
+    if (computerSelection == "rock") {
+        srcComputer = "piedra.png"
+    } else if (computerSelection == "scissor") {
+        srcComputer = "tijera.png"
+    } else {
+        srcComputer = "papel.png"
+    }
+
+    spanScreen.innerHTML = `<img src="${playerSelection}" /> <h3>VS</h3>   <img src="${srcComputer}" />   `
+
+}
 
 
 
